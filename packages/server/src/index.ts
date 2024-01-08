7328da10ad41ce41d653c2efb28d6800c30f50cc
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-01-08 15:50:25 +0530

    Added Prettier  and Eslint

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,16 @@
-import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
-dotenv.config();
+const app = express()
+dotenv.config()
 
-app.use(cors());
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port ${process.env.PORT}`);
-});
+  console.log(`Server listening on port ${process.env.PORT}`)
+})
