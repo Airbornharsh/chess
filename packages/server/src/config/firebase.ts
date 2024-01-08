6fec8d4404ce408803c00ab6ba1b2cf75777891a
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-01-08 17:50:08 +0530

    added Firebase Configs and Env  in both server and web

---
+++ packages/server/src/config/firebase.ts
@@ -0,0 +1,14 @@
+import admin from 'firebase-admin'
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  }),
+})
+
+const firestore = admin.firestore()
+const auth = admin.auth()
+
+export { firestore, auth }
